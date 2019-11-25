069e8a570db56eb17d8e69b5c988ee010acf5d88
Александр Артюхин <artuhin2010@icloud.com>
2019-11-25 20:18:58 +0300

    DevNews_v1.0

---
+++ FitnessGym/News/NewsControllerVC.swift
@@ -0,0 +1,21 @@
+//
+//  NewsControllerVC.swift
+//  FitnessGym
+//
+//  Created by Александр Артюхин on 25.11.2019.
+//  Copyright © 2019 Grinevich Sergey. All rights reserved.
+//
+
+import UIKit
+
+class NewsControllerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("hello word")
+    }
+    
+
+
+}
