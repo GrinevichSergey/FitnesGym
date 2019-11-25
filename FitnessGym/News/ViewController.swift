7290a5940544062cdf10dd8a5c47394ee72c9628
Александр Артюхин <artuhin2010@icloud.com>
2019-11-25 20:06:27 +0300

    Test

---
+++ FitnessGym/News/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FitnessGym
+//
+//  Created by Александр Артюхин on 25.11.2019.
+//  Copyright © 2019 Grinevich Sergey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       print("")
+    }
+    
+
+
+}
